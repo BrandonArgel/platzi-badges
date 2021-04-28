416d39531c29d6adde53bb1ec510a1b56f5916e2
Brandon <brandargel@gmail.com>
2021-04-27 22:08:28 -0500

    Message Fetch

--- src/components/PageError.js
+++ src/components/PageError.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import './styles/PageError.css';
+import "./styles/PageError.css";
 
 function PageError(props) {
-  return <div className="PageError">❌ {props.error.message} 😱</div>;
+  // return <div className="PageError">❌ {props.error.message} 😱</div>;
+  return <div className="PageError">🛠️ Implementando Base de datos 🛠️</div>;
 }
 
 export default PageError;
