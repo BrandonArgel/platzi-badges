39cef509663513ca641e3fd5a632bb817c187036
BrandonArgel <brandargel@gmail.com>
2021-09-02 12:53:29 -0500

    Updates

---
+++ precache-manifest.65b2f6bacda9da5dd08390678f1333b0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18d65decbc81beab1f6f987f135372e",
+    "url": "/platzi-badges/index.html"
+  },
+  {
+    "revision": "4bb2bc337249e82c2757",
+    "url": "/platzi-badges/static/css/2.d805e148.chunk.css"
+  },
+  {
+    "revision": "17577a725143eb53e12e",
+    "url": "/platzi-badges/static/css/main.c4ee72cd.chunk.css"
+  },
+  {
+    "revision": "4bb2bc337249e82c2757",
+    "url": "/platzi-badges/static/js/2.6db70ac2.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/platzi-badges/static/js/2.6db70ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17577a725143eb53e12e",
+    "url": "/platzi-badges/static/js/main.179d5279.chunk.js"
+  },
+  {
+    "revision": "60ff945f6066fe8d98da",
+    "url": "/platzi-badges/static/js/runtime-main.a9177a99.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "fa5992b7388f75d043fbd4cddf3c47af",
+    "url": "/platzi-badges/static/media/not_found.fa5992b7.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "b3095839f96e3e9ca01c1a564347a68a",
+    "url": "/platzi-badges/static/media/server_error.b3095839.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/platzi-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "b7cef21b3b9d3628c64b3bf6e256d58e",
+    "url": "/platzi-badges/static/media/twitter.b7cef21b.png"
+  }
+]);
